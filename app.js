730d8889e0488bd8dc2e902a77c81a28aef8a9fa
Vathan Silva <vathansilva@gmail.com>
2023-07-07 13:51:09 +0530

    francis intitial commit

---
+++ app.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+const app = express();
+require('dotenv').config()
+
+app.use(express.json());
+app.use(cors());
+
+const db = mysql.createConnection({
+  host:"localhost",
+  user:"root",
+  password:"h*9I35iub1U^",
+  database:"movie-crud"
+});
+
+const port = process.env.PORT || 8206;
+
+app.listen(port, ()=>{
+  console.log("Connected to backend")
+});
+
+app.get(`/`, (req, res) => {
+  res.json("Hello this is backend");
+});
+
+app.get("/movies", (req,res)=>{
+  const q = "SELECT * FROM movies"
+  db.query(q,(err,data)=>{
+    if(err) return res.json(err)
+    return res.json(data)
+  })
+})
+
+app.post("/movies", (req, res) => {
+  const q = "INSERT INTO movies (`movieName`,`director`,`budget`,`moviePic`,`imdbrate`) VALUES (?)";
+  const values = [
+    req.body.movieName,
+    req.body.director,
+    req.body.budget,
+    req.body.moviePic,
+    req.body.imdbrate,
+  ];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Movie has been created successfully.");
+  });
+});
+
+app.delete("/movies/:id", (req, res) => {
+  const movieId = req.params.id;
+  const q = "DELETE FROM movies WHERE id = ?";
+
+  db.query(q, [movieId], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Movie has been deleted successfully.");
+  });
+});
+
+app.put("/movies/:id", (req, res) => {
+  const movieId = req.params.id;
+  const q = "UPDATE movies SET `movieName` = ?, `director` = ?, `budget` = ?, `moviePic` = ?, `imdbrate` = ? WHERE id = ?";
+
+  const values = [
+    req.body.movieName,
+    req.body.director,
+    req.body.budget,
+    req.body.moviePic,
+    req.body.imdbrate,
+  ];
+
+  db.query(q, [...values,movieId], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Movie has been updated successfully.");
+  });
+});
+
+//404-not found
+app.use((req, res) => {
+  res.status(404).send('Path not found');
+});
